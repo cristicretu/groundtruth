639f3e14861ac4a0073f54a69a59c2f6b2d4fef1
Cristian Cretu <crisemcr@gmail.com>
2025-12-30 11:11:08 +0200

    Initial Commit

---
+++ groundtruth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  groundtruth
+//
+//  Created by John Doe on 30.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ groundtruth/groundtruthApp.swift
@@ -0,0 +1,17 @@
+//
+//  groundtruthApp.swift
+//  groundtruth
+//
+//  Created by John Doe on 30.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct groundtruthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
